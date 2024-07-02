42a3e542230481556d7116bc200ebfd85007853f
mojulian <mojulian@student.ethz.ch>
2024-07-02 10:27:26 +0200

    supporting versioning and experiment id

--- a_train_export.py
+++ a_train_export.py
@@ -1,15 +1,31 @@
 import torch 
 from ultralytics import YOLO 
+import wandb
+
+load = False
+exp_id = 'exp1'
+
+version = 'v8'
+
+if version == 'v1':
+    print('Please, check to modify ultralytics/nn/modules/head/Detect')
+    print('for TinyissimoYOLOv1.3 small and big change')
+    print('line 36 to: self.reg_max=16')
+    exit(1)
 
 device = torch.device("cuda")
-model_name = "./ultralytics/cfg/models/tinyissimo/tinyissimo-v8.yaml"
-model = YOLO(model_name) 
+if load:
+    model_name = f'./results/{exp_id}/weights/last.pt'
+    model = YOLO(model_name) 
+else:
+    model_name = f"./ultralytics/cfg/models/tinyissimo/tinyissimo-{version}.yaml"
+    model = YOLO(model_name) 
 
-img_size = 256
+img_size = 224
 input_size = (1, 1, img_size, img_size)  
  
 # Train
-model.train(data="RATS.yaml",  project="results", name="exp", optimizer='SGD',  imgsz=img_size,  epochs=100,  batch=64)
+model.train(data="coco.yaml",  project="results", name="exp", optimizer='SGD',  imgsz=img_size,  epochs=1000,  batch=512)
 
 # Export
 model.export(format="onnx", project="results", name="exp", imgsz=[img_size,img_size]) 
